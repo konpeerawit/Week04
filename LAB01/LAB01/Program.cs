868cfd2cdf2285d00a0fea472106f9c7b7de4ea6
Kon-NB <Kon-NB@DESKTOP-5MGOQA6>
2017-09-07 14:02:44 +0700

    Week04P04

--- LAB01/LAB01/Program.cs
+++ LAB01/LAB01/Program.cs
@@ -6,7 +6,7 @@ namespace LAB01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("{0}and{1}",3,6);
+            Console.WriteLine(" {1},{0} and {1}", 3, 6);
         }
     }
 }
